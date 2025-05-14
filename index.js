51765483480e1f7e5e90a1fc901e8c97d58831e6
Faisal Chowdhury <chowdhuryfaisal66@gmail.com>
2025-05-14 23:52:39 +0600

    Database connected and Add User api created

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
-const express = require('express');
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
-const cors = require('cors');
-
+const cors = require("cors");
 app.use(cors());
+app.use(express.json());
 
-app.get('/' , (req , res)=> {
-    res.send('server is working')
-})
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.0o3jxdg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-app.listen(port , ()=> {
-    console.log('server running on port ' , port);
-})
+async function run() {
+  try {
+    await client.connect();
+    await client.db("user_profile").command({ ping: 1 });
+    console.log("pinged ,Db Connected");
+
+    const db = client.db("user_profile");
+    const collection = db.collection("user");
+
+    // Add User
+
+    app.post("/users", async (req, res) => {
+      const document = req.body;
+      const result = await collection.insertOne(document);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("server is working");
+});
+
+app.listen(port, () => {
+  console.log("server running on port ", port);
+});
